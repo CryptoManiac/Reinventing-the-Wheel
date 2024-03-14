55a6e237aa23c22a044431aae5916fe02ed1f988
CryptoManiac <balthazar.ad@gmail.com>
2024-03-15 02:05:34 +0300

    Sign function

--- Wheel/Crypto/Elliptic/SECP256K1/ECKey.cs
+++ Wheel/Crypto/Elliptic/SECP256K1/ECKey.cs
@@ -1,10 +1,18 @@
-﻿using Wheel.Crypto.Elliptic.Internal.SECP256K1;
+﻿using System.Runtime.InteropServices;
+using Wheel.Crypto.Elliptic.Internal.SECP256K1;
 using Wheel.Crypto.Elliptic.Internal.VeryLongInt;
+using Wheel.Crypto.Hashing.HMAC;
 
 namespace Wheel.Crypto.Elliptic.SECP256K1
 {
     public static class ECKey
     {
+        /// <summary>
+        /// Compute the corresponding public key for a private key.
+        /// </summary>
+        /// <param name="result">Will be filled in with the corresponding public key</param>
+        /// <param name="private_key"> The private key to compute the public key for</param>
+        /// <returns>True if the key was computed successfully, False if an error occurred.</returns>
         public static bool ComputePublicKey(Span<ulong> result, ReadOnlySpan<ulong> private_key)
         {
             Span<ulong> tmp1 = stackalloc ulong[VLI_Common.ECC_MAX_WORDS];
@@ -103,6 +111,13 @@ namespace Wheel.Crypto.Elliptic.SECP256K1
             VLI_Conversion.NativeToBytes(public_key.Slice(Constants.NUM_N_BYTES), Constants.NUM_N_BYTES, y);
         }
 
+        /// <summary>
+        /// Private key tweak by scalar
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="private_key"></param>
+        /// <param name="scalar"></param>
+        /// <returns></returns>
         public static bool PrivateKeyTweak(Span<byte> result, ReadOnlySpan<byte> private_key, ReadOnlySpan<byte> scalar)
         {
             Span<ulong> _private = stackalloc ulong[VLI_Common.ECC_MAX_WORDS];
@@ -154,6 +169,13 @@ namespace Wheel.Crypto.Elliptic.SECP256K1
             return true;
         }
 
+        /// <summary>
+        /// EC public key tweak by scalar
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="public_key"></param>
+        /// <param name="scalar"></param>
+        /// <returns></returns>
         public static bool PublicKeyTweak(Span<byte> result, ReadOnlySpan<byte> public_key, ReadOnlySpan<byte> scalar)
         {
             Span<ulong> _public = stackalloc ulong[VLI_Common.ECC_MAX_WORDS * 2];
@@ -190,6 +212,164 @@ namespace Wheel.Crypto.Elliptic.SECP256K1
             VLI_Conversion.NativeToBytes(result.Slice(Constants.NUM_N_BYTES), Constants.NUM_N_BYTES, _result.Slice(Constants.NUM_WORDS));
 
             return true;
+        }
+
+        /// <summary>
+        /// Generate an ECDSA signature for a given hash value, with the user-provided provided K
+        /// </summary>
+        /// <param name="signature">Will be filled in with the signature value</param>
+        /// <param name="private_key">Your private key</param>
+        /// <param name="message_hash">The hash of the message to sign</param>
+        /// <param name="K">Random secret</param>
+        /// <returns></returns>
+        public static bool SignWithK(Span<byte> signature, ReadOnlySpan<byte> private_key, ReadOnlySpan<byte> message_hash, ReadOnlySpan<ulong> K)
+        {
+            const int num_words = Constants.NUM_WORDS;
+            const int num_bytes = Constants.NUM_N_BYTES;
+            const int num_n_words = Constants.NUM_WORDS;
+            const int num_n_bits = Constants.NUM_N_BITS;
+
+            ulong carry;
+
+            Span<ulong> p = stackalloc ulong[num_words * 2];
+            Span<ulong> s = stackalloc ulong[num_words];
+            Span<ulong> tmp = stackalloc ulong[num_words];
+            VLI_Common.Picker<ulong> k2 = new(tmp, s);
+
+            // Make a local copy of K for in-place modification
+            Span<ulong> k = stackalloc ulong[num_words];
+            VLI_Arithmetic.Set(k, K, num_words);
+
+            // Make sure 0 < k < curve_n
+            if (VLI_Logic.IsZero(k, num_words) || VLI_Logic.Cmp(Constants.n, k, num_n_words) != 1)
+            {
+                return false;
+            }
+
+            carry = ECCUtil.regularize_k(k, tmp, s);
+            ECCPoint.PointMul(p, Constants.G, k2[VLI_Logic.ZeroIfNotZero(carry)], num_n_bits + 1);
+            if (VLI_Logic.IsZero(p, num_words))
+            {
+                return false;
+            }
+
+            // Stub: No RNG function is actually used here
+            VLI_Arithmetic.Clear(tmp, num_n_words);
+            tmp[0] = 1;
+
+            // Prevent side channel analysis of uECC_vli_modInv() to determine
+            //   bits of k / the private key by premultiplying by a random number
+            VLI_Arithmetic.ModMult(k, k, tmp, Constants.n, num_n_words); // k' = rand * k
+            VLI_Arithmetic.ModInv(k, k, Constants.n, num_n_words);       // k = 1 / k'
+            VLI_Arithmetic.ModMult(k, k, tmp, Constants.n, num_n_words); // k = 1 / k
+
+            VLI_Conversion.NativeToBytes(signature, num_bytes, p); // store r
+            VLI_Conversion.BytesToNative(tmp, private_key, num_bytes); // tmp = d
+
+            s[num_n_words - 1] = 0;
+            VLI_Arithmetic.Set(s, p, num_words);
+            VLI_Arithmetic.ModMult(s, tmp, s, Constants.n, num_n_words); // s = r*d
+
+            BitsToInt(tmp, message_hash, message_hash.Length);
+
+            VLI_Arithmetic.ModAdd(s, tmp, s, Constants.n, num_n_words); // s = e + r*d
+            VLI_Arithmetic.ModMult(s, s, k, Constants.n, num_n_words);  // s = (e + r*d) / k
+
+            if (VLI_Logic.NumBits(s, num_n_words) > num_n_bits)
+            {
+                return false;
+            }
+
+            if (VLI_Logic.Cmp(s, Constants.half_n, num_words) == 1)
+            {
+                // Apply Low-S rule to signature
+                VLI_Arithmetic.Sub(s, Constants.n, s, num_words); // s = n - s
+            }
+
+            VLI_Conversion.NativeToBytes(signature.Slice(num_bytes), num_bytes, s);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Generate an ECDSA signature for a given hash value, using a deterministic algorithm
+        /// (see RFC 6979).
+        /// 
+        /// Usage: Compute a hash of the data you wish to sign and pass it to
+        /// this function along with your private key and a hash context. Note that the message_hash
+        /// does not need to be computed with the same hash function used by hash_context.
+        /// </summary>
+        /// <param name="signature">Will be filled in with the signature value</param>
+        /// <param name="private_key">Your private key</param>
+        /// <param name="message_hash">The hash of the message to sign</param>
+        /// <param name="hasher">A hasher to use</param>
+        /// <returns></returns>
+        public static bool SignDeterministic(Span<byte> signature, ReadOnlySpan<byte> private_key, ReadOnlySpan<byte> message_hash, IMac hasher)
+        {
+            // Allocate buffer for HMAC results
+            Span<byte> K = stackalloc byte[hasher.HashSz];
+
+            // Sequence of iteration is encoded here
+            Span<byte> n = stackalloc byte[sizeof(long)];
+
+            // Will retry until succeed
+            for (long i = 0; ; ++i)
+            {
+
+                // Init HMAC with private key
+                hasher.Init(private_key);
+
+                // Add message hash
+                hasher.Update(message_hash);
+
+                // Encode and add iteration sequence
+                MemoryMarshal.Cast<byte, long>(n)[0] = i;
+                hasher.Update(n);
+
+                // Hash is then used as K parameter
+                hasher.Digest(K);
+
+                // Try to sign
+                if (SignWithK(signature, private_key, message_hash, MemoryMarshal.Cast<byte, ulong>(K)))
+                {
+                    return true;
+                }
+            }
+        }
+
+        private static void BitsToInt(Span<ulong> native, ReadOnlySpan<byte> bits, int bits_size)
+        {
+            const int num_n_bytes = Constants.NUM_N_BYTES;
+            const int num_n_words = Constants.NUM_WORDS;
+            const int num_n_bits = Constants.NUM_N_BITS;
+
+            if (bits_size > num_n_bytes)
+            {
+                bits_size = num_n_bytes;
+            }
+
+            VLI_Arithmetic.Clear(native, num_n_words);
+            VLI_Conversion.BytesToNative(native, bits, bits_size);
+            if (bits_size * 8 <= num_n_bits)
+            {
+                return;
+            }
+
+            ulong carry = 0;
+            int index = num_n_words - 1;
+            int shift = bits_size * 8 - num_n_bits;
+            while (index-- >= 0)
+            {
+                ulong temp = native[index];
+                native[index] = (temp >> shift) | carry;
+                carry = temp << (VLI_Common.WORD_BITS - shift);
+            }
+
+            /* Reduce mod curve_n */
+            if (VLI_Logic.CmpUnsafe(Constants.n, native, num_n_words) != 1)
+            {
+                VLI_Arithmetic.Sub(native, native, Constants.n, num_n_words);
+            }
         }
     }
 }
