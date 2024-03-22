5571b328ee94d3f633e28b6a5396445aa788b6d8
CryptoManiac <balthazar@yandex.ru>
2024-03-22 21:43:53 +0300

    Base58 codec: Alphabet argument for constructor

---
+++ EncodersTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using Wheel.Encoders;
+
+// Sample data from this wiki page: https://en.bitcoin.it/wiki/Technical_background_of_version_1_Bitcoin_addresses
+Span<byte> dataToEncode = stackalloc byte[] { 0x00, 0xf5, 0x4a, 0x58, 0x51, 0xe9, 0x37, 0x2b, 0x87, 0x81, 0x0a, 0x8e, 0x60, 0xcd, 0xd2, 0xe7, 0xcf, 0xd8, 0x0b, 0x6e, 0x31, 0xc7, 0xf1, 0x8f, 0xe8 };
+string b58expected = "1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs";
+
+Console.WriteLine("Data to encode: {0}", Convert.ToHexString(dataToEncode));
+
+/// Instantiate new codec
+Base58Codec codec = new Base58Codec();
+
+/// Buffer for base58 encoding result
+Span<char> b58chr = stackalloc char[codec.Encode(null, dataToEncode)];
+
+{
+    int encoded = codec.Encode(b58chr, dataToEncode);
+    b58chr = b58chr.Slice(0, encoded);
+}
+
+if (!b58chr.SequenceEqual(b58expected))
+{
+    throw new SystemException("Encoded string mismatch");
+}
+
+Console.WriteLine("Encoding result: {0}", b58chr.ToString());
+
+/// Buffer for base58 decoding result
+Span<byte> b58data = stackalloc byte[codec.Decode(null, b58chr)];
+
+{
+    int decoded = codec.Decode(b58data, b58chr);
+    b58data = b58data.Slice(0, decoded);
+}
+
+if (!b58data.SequenceEqual(dataToEncode))
+{
+    throw new SystemException("Decoded data mismatch");
+}
+
+Console.WriteLine("Decoding result: {0}", Convert.ToHexString(b58data));
---
+++ Wheel/Encoders/Base58.cs
@@ -0,0 +1,205 @@
+﻿namespace Wheel.Encoders
+{
+    /// <summary>
+    /// Base58 encoding and decoding engine.
+    /// The instances are guaranteed to have a deterministic memory size and executions of their methods are guaranteed to not result in allocation of any heap memory.
+    /// Note: Based on this C++ implementation: https://bitcoin.stackexchange.com/a/96359
+    /// </summary>
+    public struct Base58Codec
+    {
+        /// <summary>
+        /// Base58 alphabet as defined by the Bitcoin
+        /// </summary>
+        private static readonly char[] StaticBase58Map =
+        [
+          '1', '2', '3', '4', '5', '6', '7', '8',
+          '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+          'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q',
+          'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+          'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+          'h', 'i', 'j', 'k', 'm', 'n', 'o', 'p',
+          'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+          'y', 'z'
+        ];
+
+        /// <summary>
+        /// Base58 character mapping table
+        /// </summary>
+        private static readonly byte[] StaticAlphaMap =
+        [
+          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0xff, 0x11, 0x12, 0x13, 0x14, 0x15, 0xff,
+          0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0xff, 0xff, 0xff, 0xff, 0xff,
+          0xff, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0xff, 0x2c, 0x2d, 0x2e,
+          0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0xff, 0xff, 0xff, 0xff, 0xff
+        ];
+
+        /// <summary>
+        /// On-stack copy of alphabet characters
+        /// </summary>
+        private unsafe fixed char Base58Map[58];
+
+        /// <summary>
+        /// On-stack copy of character mapping
+        /// </summary>
+        private unsafe fixed byte AlphaMap[256];
+
+        public unsafe Base58Codec(ReadOnlySpan<char> alphabet)
+        {
+            if (!CheckAlphabet(alphabet))
+            {
+                throw new ArgumentException("Incorrect alphabet");
+            }
+
+            if (alphabet.Length != 58)
+            {
+                throw new ArgumentException("The base58 alphabet is expected to be 58 characters long");
+            }
+
+            fixed (char* ptr = &Base58Map[0])
+            {
+                alphabet.CopyTo(new Span<char>(ptr, 58));
+            }
+
+            fixed (byte* ptr = &AlphaMap[0])
+            {
+                StaticAlphaMap.CopyTo(new Span<byte>(ptr, 256));
+            }
+        }
+
+        /// <summary>
+        /// Alphabet requirements:
+        /// * Must not contain any duplicates
+        /// * Must not contain any non-ASCII characters
+        /// </summary>
+        /// <param name="alphabet"></param>
+        /// <returns>True if the alphabet is valid</returns>
+        public static bool CheckAlphabet(ReadOnlySpan<char> alphabet)
+        {
+            uint bits = 0;
+
+            foreach (var c in alphabet)
+            {
+                if (!char.IsAscii(c)) {
+                    return false; 
+                }
+
+                uint b = bits << c;
+                uint next_bits = bits ^ b;
+                if (next_bits < bits)
+                {
+                    return false;
+                }
+                bits = next_bits;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Construct a new instance of codec by filling the buffers 
+        /// </summary>
+        public unsafe Base58Codec() : this(StaticBase58Map)
+        {
+        }
+
+        /// <summary>
+        /// Encode the contents of byte buffer and write the resulting characters into provided buffer
+        /// </summary>
+        /// <param name="result">Character buffer to fill</param>
+        /// <param name="data">Data to encode</param>
+        /// <returns>Number of written characters, if execution was successful. The required buffer length, if not.</returns>
+        public unsafe int Encode(Span<char> result, ReadOnlySpan<byte> data)
+        {
+            // For the worst case
+            Span<byte> b256 = stackalloc byte[(data.Length * 138 / 100) + 1];
+            int b256Sz = 1;
+            for (int i = 0; i < data.Length; ++i)
+            {
+                uint carry = data[i];
+                for (int j = 0; j < b256Sz; ++j)
+                {
+                    carry += (uint)(b256[j] << 8);
+                    b256[j] = (byte)(carry % 58);
+                    carry /= 58;
+                }
+                for (; Convert.ToBoolean(carry); carry /= 58)
+                    b256[b256Sz++] = (byte)(carry % 58);
+            }
+
+            int reqSz = data.Length + b256Sz;
+
+            if (result.Length >= reqSz)
+            {
+                fixed(char* ptr = &result[0])
+                {
+                    char* current = ptr;
+
+                    for (int i = 0; i < (data.Length - 1) && !Convert.ToBoolean(data[i]); ++i, ++current)
+                    {
+                        *current = Base58Map[0];
+                    }
+
+                    for (int i = 0; i < b256Sz; ++i, ++current)
+                    {
+                        *current = Base58Map[b256[b256Sz - 1 - i]];
+                    }
+
+                    // Note: the char values are always one
+                    //   byte long in our case.
+                    return (int)(current - ptr);
+                }
+            }
+
+            // Caller must provide a buffer with the sufficient space
+            //  for the result characters
+            return reqSz;
+        }
+
+        /// <summary>
+        /// Decode characters and write the resulting bytes into buffer
+        /// </summary>
+        /// <param name="result">Byte buffer to fill with the decoded bytes</param>
+        /// <param name="data">Characters buffer to decode</param>
+        /// <returns>Number of written bytes, if execution was successful. The required buffer length, if not.</returns>
+        public unsafe int Decode(Span<byte> result, ReadOnlySpan<char> data)
+        {
+            // For the worst case
+            int reqSz = (data.Length * 138 / 100) + 1;
+
+            if (result.Length < reqSz)
+            {
+                // Caller must provide a buffer with the sufficient space
+                //  for the result bytes
+                return reqSz;
+            }
+
+            int byteSz = 1;
+            for (int i = 0; i < data.Length; ++i)
+            {
+                uint carry = AlphaMap[data[i] & 0x7f];
+                for (int j = 0; j < byteSz; j++, carry >>= 8)
+                {
+                    carry += (uint)(result[j] * 58);
+                    result[j] = (byte)carry;
+                }
+                for (; Convert.ToBoolean(carry); carry >>= 8)
+                {
+                    result[byteSz++] = (byte)carry;
+                }
+            }
+
+            int k = 0;
+            for (; k < (data.Length - 1) && data[k] == Base58Map[0]; ++k)
+            {
+                result[byteSz + k] = 0;
+            }
+
+            result.Slice(0, byteSz + k).Reverse();
+            return byteSz + k;
+        }
+    }
+}
