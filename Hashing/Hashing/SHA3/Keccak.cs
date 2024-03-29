eb1fdd7193ec2411c37ab2b9c054ab61e167a0b1
CryptoManiac <balthazar.ad@gmail.com>
2024-03-29 07:53:23 +0300

    Refactor Keccak to make it rely less on the unsafe code

--- Hashing/Hashing/SHA3/Keccak.cs
+++ Hashing/Hashing/SHA3/Keccak.cs
@@ -7,48 +7,62 @@ namespace Wheel.Hashing.SHA3.Internal
     internal struct Keccak : IHasher
     {
         /// <summary>
-        /// the double size of the hash output in
-        /// words (e.g. 16 for Keccak 512)
-        /// </summary>
-        [FieldOffset(0)]
-        public readonly uint capacityWords;
-
-        /// <summary>
         /// 0..7--the next byte after the set one
         /// (starts from 0; 0--none are buffered)
         /// </summary>
-        [FieldOffset(4)]
+        [FieldOffset(0)]
         public int byteIndex;
 
         /// <summary>
         /// 0..24--the next word to integrate input
         /// (starts from 0)
         /// </summary>
-        [FieldOffset(8)]
-        public uint wordIndex;
+        [FieldOffset(4)]
+        public int wordIndex;
 
         /// <summary>
         /// the portion of the input message that we didn't consume yet
         /// </summary>
-        [FieldOffset(12)]
+        [FieldOffset(8)]
         public ulong saved;
 
         /// <summary>
         /// Keccak data mixer
         /// </summary>
-        [FieldOffset(20)]
-        private unsafe fixed ulong registers[SHA3_SPONGE_WORDS];
+        [FieldOffset(16)]
+        private unsafe fixed ulong words[SHA3_SPONGE_WORDS];
 
         /// <summary>
-        /// A local copy of round constants
+        /// the double size of the hash output in
+        /// words (e.g. 16 for Keccak 512)
         /// </summary>
-        [FieldOffset(20 + SHA3_SPONGE_WORDS * sizeof(ulong))]
-        private unsafe fixed ulong keccakf_rndc[24];
+        [FieldOffset(216)]
+        public readonly int capacityWords;
+
+        /// <summary>
+        /// Keccak is a little different from the standard SHA3
+        /// </summary>
+        [FieldOffset(220)]
+        public readonly bool IsKeccak;
+
+        /// <summary>
+        /// To avoid the unsafe blocks where the performance is not critical
+        /// </summary>
+        private readonly unsafe Span<ulong> registers
+        {
+            get
+            {
+                fixed (ulong* ptr = &words[0])
+                {
+                    return new Span<ulong>(ptr, SHA3_SPONGE_WORDS);
+                }
+            }
+        }
 
         /// <summary>
         /// Keccak round constants array
         /// </summary>
-        private static ulong[] rndc = new ulong[24] {
+        private static readonly ulong[] rndc = new ulong[24] {
                 0x0000000000000001, 0x0000000000008082, 0x800000000000808a,
                 0x8000000080008000, 0x000000000000808b, 0x0000000080000001,
                 0x8000000080008081, 0x8000000000008009, 0x000000000000008a,
@@ -60,48 +74,34 @@ namespace Wheel.Hashing.SHA3.Internal
         };
 
         public const int SHA3_SPONGE_WORDS = 25; // Calculated as 1600 / 8 / sizeof(ulong)
-        public const uint SHA3_USE_KECCAK_FLAG = 0x80000000;
-
-        public static uint SHA3_CW(uint x) => x & (~SHA3_USE_KECCAK_FLAG);
-
-        public readonly bool IsKeccak
-        {
-            get { return 0 != (capacityWords & SHA3_USE_KECCAK_FLAG); }
-        }
 
         public readonly int HashSz
         {
             get { return (int)capacityWords * 4; }
         }
 
-        public unsafe Keccak(int bitSize, bool isKeccak)
+        public Keccak(int bitSize, bool isKeccak)
         {
             if (bitSize != 256 && bitSize != 384 && bitSize != 512)
             {
                 throw new InvalidOperationException("Valid bitSize values are: 256, 384 or 512");
             }
 
-            capacityWords = (uint)bitSize / 32;
-
-            if (isKeccak)
-            {
-                capacityWords |= SHA3_USE_KECCAK_FLAG;
-            }
-
-            #region Init and copy round constants
-            fixed (ulong* ptr = &this.keccakf_rndc[0])
-            {
-                rndc.CopyTo(new Span<ulong>(ptr, rndc.Length));
-            }
-            #endregion
+            capacityWords = bitSize / 32;
+            IsKeccak = isKeccak;
         }
 
-        public unsafe void Reset()
+        public void Reset()
         {
-            fixed(void* ptr = &this)
+            byteIndex = 0;
+            wordIndex = 0;
+            saved = 0;
+            unsafe
             {
-                // Skip the first 4 bytes to keep the capacityWords intact
-                new Span<byte>((byte*)ptr + sizeof(uint), sizeof(Keccak) - sizeof(uint)).Clear();
+                fixed (void* ptr = &registers[0])
+                {
+                    new Span<ulong>(ptr, SHA3_SPONGE_WORDS).Clear();
+                }
             }
         }
 
@@ -114,19 +114,16 @@ namespace Wheel.Hashing.SHA3.Internal
             // now work in full words directly from input
             for (int i = 0; i < words; i++, offset += 8)
             {
-                unsafe
-                {
-                    registers[wordIndex] ^= (input[offset]) |
-                        ((ulong)input[offset + 1] << 8 * 1) |
-                        ((ulong)input[offset + 2] << 8 * 2) |
-                        ((ulong)input[offset + 3] << 8 * 3) |
-                        ((ulong)input[offset + 4] << 8 * 4) |
-                        ((ulong)input[offset + 5] << 8 * 5) |
-                        ((ulong)input[offset + 6] << 8 * 6) |
-                         ((ulong)input[offset + 7] << 8 * 7);
-                }
+                registers[wordIndex] ^= (input[offset]) |
+                    ((ulong)input[offset + 1] << 8 * 1) |
+                    ((ulong)input[offset + 2] << 8 * 2) |
+                    ((ulong)input[offset + 3] << 8 * 3) |
+                    ((ulong)input[offset + 4] << 8 * 4) |
+                    ((ulong)input[offset + 5] << 8 * 5) |
+                    ((ulong)input[offset + 6] << 8 * 6) |
+                     ((ulong)input[offset + 7] << 8 * 7);
 
-                if (++wordIndex == (SHA3_SPONGE_WORDS - SHA3_CW(capacityWords)))
+                if (++wordIndex == (SHA3_SPONGE_WORDS - capacityWords))
                 {
                     KeccakF();
                     wordIndex = 0;
@@ -149,7 +146,7 @@ namespace Wheel.Hashing.SHA3.Internal
             {
                 // have no complete word or haven't started
                 // the word yet
-                foreach(var b in input)
+                foreach (var b in input)
                 {
                     saved |= (ulong)b << (byteIndex++ * 8);
                 }
@@ -165,15 +162,12 @@ namespace Wheel.Hashing.SHA3.Internal
 
                 // now ready to add saved to the sponge
 
-                unsafe
-                {
-                    registers[wordIndex] ^= saved;
-                }
+                registers[wordIndex] ^= saved;
 
                 byteIndex = 0;
                 saved = 0;
 
-                if (++wordIndex == (SHA3_SPONGE_WORDS - SHA3_CW(capacityWords)))
+                if (++wordIndex == (SHA3_SPONGE_WORDS - capacityWords))
                 {
                     KeccakF();
                     wordIndex = 0;
@@ -215,11 +209,8 @@ namespace Wheel.Hashing.SHA3.Internal
                 t = ((ulong)(0x02 | (1 << 2))) << (byteIndex * 8);
             }
 
-            unsafe
-            {
-                registers[wordIndex] ^= saved ^ t;
-                registers[SHA3_SPONGE_WORDS - SHA3_CW(capacityWords) - 1] ^= 0x8000000000000000UL;
-            }
+            registers[wordIndex] ^= saved ^ t;
+            registers[SHA3_SPONGE_WORDS - capacityWords - 1] ^= 0x8000000000000000UL;
 
             KeccakF();
 
@@ -227,22 +218,13 @@ namespace Wheel.Hashing.SHA3.Internal
             //  Considering that Itanium is dead, this is unlikely to ever be useful
             if (!BitConverter.IsLittleEndian)
             {
-                for (uint i = 0; i < SHA3_SPONGE_WORDS; ++i)
+                for (int i = 0; i < SHA3_SPONGE_WORDS; ++i)
                 {
-                    unsafe
-                    {
-                        Common.REVERT(ref registers[i]);
-                    }
+                    Common.REVERT(ref registers[i]);
                 }
             }
 
-            unsafe
-            {
-                fixed (void* source = &registers[0])
-                {
-                    new Span<byte>(source, hash.Length).CopyTo(hash);
-                }
-            }
+            MemoryMarshal.Cast<ulong, byte>(registers).Slice(0, hash.Length).CopyTo(hash);
 
             // Reset hasher state
             Reset();
@@ -266,11 +248,11 @@ namespace Wheel.Hashing.SHA3.Internal
             for (int round = 0; round < 24; ++round)
             {
                 // Theta
-                bc0 = registers[0] ^ registers[5 + 0] ^ registers[10 + 0] ^ registers[15 + 0] ^ registers[20 + 0];
-                bc1 = registers[1] ^ registers[5 + 1] ^ registers[10 + 1] ^ registers[15 + 1] ^ registers[20 + 1];
-                bc2 = registers[2] ^ registers[5 + 2] ^ registers[10 + 2] ^ registers[15 + 2] ^ registers[20 + 2];
-                bc3 = registers[3] ^ registers[5 + 3] ^ registers[10 + 3] ^ registers[15 + 3] ^ registers[20 + 3];
-                bc4 = registers[4] ^ registers[5 + 4] ^ registers[10 + 4] ^ registers[15 + 4] ^ registers[20 + 4];
+                bc0 = words[0] ^ words[5 + 0] ^ words[10 + 0] ^ words[15 + 0] ^ words[20 + 0];
+                bc1 = words[1] ^ words[5 + 1] ^ words[10 + 1] ^ words[15 + 1] ^ words[20 + 1];
+                bc2 = words[2] ^ words[5 + 2] ^ words[10 + 2] ^ words[15 + 2] ^ words[20 + 2];
+                bc3 = words[3] ^ words[5 + 3] ^ words[10 + 3] ^ words[15 + 3] ^ words[20 + 3];
+                bc4 = words[4] ^ words[5 + 4] ^ words[10 + 4] ^ words[15 + 4] ^ words[20 + 4];
 
                 t0 = (bc0 << 1) ^ (bc0 >>> (64 - 1)) ^ bc3;
                 t1 = (bc1 << 1) ^ (bc1 >>> (64 - 1)) ^ bc4;
@@ -279,54 +261,54 @@ namespace Wheel.Hashing.SHA3.Internal
                 t4 = (bc4 << 1) ^ (bc4 >>> (64 - 1)) ^ bc2;
 
                 //theta (xorring part) + rho + pi
-                registers[0] ^= t1;
-                x = registers[1] ^ t2; a_10_ = (x << 1) | (x >>> (64 - 1));
-                x = registers[6] ^ t2; registers[1] = (x << 44) | (x >>> (64 - 44));
-                x = registers[9] ^ t0; registers[6] = (x << 20) | (x >>> (64 - 20));
-                x = registers[22] ^ t3; registers[9] = (x << 61) | (x >>> (64 - 61));
+                words[0] ^= t1;
+                x = words[1] ^ t2; a_10_ = (x << 1) | (x >>> (64 - 1));
+                x = words[6] ^ t2; words[1] = (x << 44) | (x >>> (64 - 44));
+                x = words[9] ^ t0; words[6] = (x << 20) | (x >>> (64 - 20));
+                x = words[22] ^ t3; words[9] = (x << 61) | (x >>> (64 - 61));
 
-                x = registers[14] ^ t0; registers[22] = (x << 39) | (x >>> (64 - 39));
-                x = registers[20] ^ t1; registers[14] = (x << 18) | (x >>> (64 - 18));
-                x = registers[2] ^ t3; registers[20] = (x << 62) | (x >>> (64 - 62));
-                x = registers[12] ^ t3; registers[2] = (x << 43) | (x >>> (64 - 43));
-                x = registers[13] ^ t4; registers[12] = (x << 25) | (x >>> (64 - 25));
+                x = words[14] ^ t0; words[22] = (x << 39) | (x >>> (64 - 39));
+                x = words[20] ^ t1; words[14] = (x << 18) | (x >>> (64 - 18));
+                x = words[2] ^ t3; words[20] = (x << 62) | (x >>> (64 - 62));
+                x = words[12] ^ t3; words[2] = (x << 43) | (x >>> (64 - 43));
+                x = words[13] ^ t4; words[12] = (x << 25) | (x >>> (64 - 25));
 
-                x = registers[19] ^ t0; registers[13] = (x << 8) | (x >>> (64 - 8));
-                x = registers[23] ^ t4; registers[19] = (x << 56) | (x >>> (64 - 56));
-                x = registers[15] ^ t1; registers[23] = (x << 41) | (x >>> (64 - 41));
-                x = registers[4] ^ t0; registers[15] = (x << 27) | (x >>> (64 - 27));
-                x = registers[24] ^ t0; registers[4] = (x << 14) | (x >>> (64 - 14));
+                x = words[19] ^ t0; words[13] = (x << 8) | (x >>> (64 - 8));
+                x = words[23] ^ t4; words[19] = (x << 56) | (x >>> (64 - 56));
+                x = words[15] ^ t1; words[23] = (x << 41) | (x >>> (64 - 41));
+                x = words[4] ^ t0; words[15] = (x << 27) | (x >>> (64 - 27));
+                x = words[24] ^ t0; words[4] = (x << 14) | (x >>> (64 - 14));
 
-                x = registers[21] ^ t2; registers[24] = (x << 2) | (x >>> (64 - 2));
-                x = registers[8] ^ t4; registers[21] = (x << 55) | (x >>> (64 - 55));
-                x = registers[16] ^ t2; registers[8] = (x << 45) | (x >>> (64 - 45));
-                x = registers[5] ^ t1; registers[16] = (x << 36) | (x >>> (64 - 36));
-                x = registers[3] ^ t4; registers[5] = (x << 28) | (x >>> (64 - 28));
+                x = words[21] ^ t2; words[24] = (x << 2) | (x >>> (64 - 2));
+                x = words[8] ^ t4; words[21] = (x << 55) | (x >>> (64 - 55));
+                x = words[16] ^ t2; words[8] = (x << 45) | (x >>> (64 - 45));
+                x = words[5] ^ t1; words[16] = (x << 36) | (x >>> (64 - 36));
+                x = words[3] ^ t4; words[5] = (x << 28) | (x >>> (64 - 28));
 
-                x = registers[18] ^ t4; registers[3] = (x << 21) | (x >>> (64 - 21));
-                x = registers[17] ^ t3; registers[18] = (x << 15) | (x >>> (64 - 15));
-                x = registers[11] ^ t2; registers[17] = (x << 10) | (x >>> (64 - 10));
-                x = registers[7] ^ t3; registers[11] = (x << 6) | (x >>> (64 - 6));
-                x = registers[10] ^ t1; registers[7] = (x << 3) | (x >>> (64 - 3));
-                registers[10] = a_10_;
+                x = words[18] ^ t4; words[3] = (x << 21) | (x >>> (64 - 21));
+                x = words[17] ^ t3; words[18] = (x << 15) | (x >>> (64 - 15));
+                x = words[11] ^ t2; words[17] = (x << 10) | (x >>> (64 - 10));
+                x = words[7] ^ t3; words[11] = (x << 6) | (x >>> (64 - 6));
+                x = words[10] ^ t1; words[7] = (x << 3) | (x >>> (64 - 3));
+                words[10] = a_10_;
 
                 // Chi
                 for (int j = 0; j < 25; j += 5)
                 {
-                    x0 = registers[j + 0];
-                    x1 = registers[j + 1];
-                    x2 = registers[j + 2];
-                    x3 = registers[j + 3];
-                    x4 = registers[j + 4];
-                    registers[j + 0] = x0 ^ ((~x1) & x2);
-                    registers[j + 1] = x1 ^ ((~x2) & x3);
-                    registers[j + 2] = x2 ^ ((~x3) & x4);
-                    registers[j + 3] = x3 ^ ((~x4) & x0);
-                    registers[j + 4] = x4 ^ ((~x0) & x1);
+                    x0 = words[j + 0];
+                    x1 = words[j + 1];
+                    x2 = words[j + 2];
+                    x3 = words[j + 3];
+                    x4 = words[j + 4];
+                    words[j + 0] = x0 ^ ((~x1) & x2);
+                    words[j + 1] = x1 ^ ((~x2) & x3);
+                    words[j + 2] = x2 ^ ((~x3) & x4);
+                    words[j + 3] = x3 ^ ((~x4) & x0);
+                    words[j + 4] = x4 ^ ((~x0) & x1);
                 }
 
                 //iota
-                registers[0] ^= keccakf_rndc[round];
+                words[0] ^= rndc[round];
             }
         }
 
